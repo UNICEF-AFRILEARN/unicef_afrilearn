416e5d642f32469852ecd00f5b97df657df02908
ISRAEL IGIETSEMHE <aloagbaye.i@gmail.com>
2022-10-13 07:39:41 -0400

    update recommender.py

--- recommender.py
+++ recommender.py
@@ -6,8 +6,8 @@ df = pickle.load(open("recommendations.pkl", 'rb'))
 def recommend(course: str, subject: str, lesson: str,length:int):
     if lesson in df['lesson_id'].unique():
         recommendation = df[(df['school_level']==course)&(df['subject']==subject)&(df['lesson_id']==lesson)]
-        recommendation = recommendation.iloc[:,0:length]
+        recommendation = recommendation.iloc[0:length,:]
     else:
         recommendation = df[(df['school_level']==course)]
-        recommendation = recommendation.iloc[:,0:length]
+        recommendation = recommendation.iloc[0:length,:]
     return recommendation
